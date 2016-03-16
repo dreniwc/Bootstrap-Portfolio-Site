b8865b5abb476c2f78fd9ed430f3ad7ecfbaeebd
Callum Dreniw <callumdreniw.bth@gmail.com>
2016-03-16 13:24:55 +0000

    Inserting a back to top button
    Using http://jsfiddle.net/panman8201/mkzrm/10/

--- js/grayscale.js
+++ js/grayscale.js
@@ -96,3 +96,12 @@ $( ".refs" ).click(function(event) {
 
 
         //lightbox script end  
+
+// Only enable if the document has a long scroll bar
+// Note the window height + offset
+if ( ($(window).height() + 100) < $(document).height() ) {
+    $('#top-link-block').removeClass('hidden').affix({
+        // how far to scroll down before link "slides" into view
+        offset: {top:100}
+    });
+}
